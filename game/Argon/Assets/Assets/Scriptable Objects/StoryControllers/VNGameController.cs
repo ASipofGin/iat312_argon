9243a6a8af20ca05693a036b7867017b232448fe
jeffrey-su <137955909+jeffrey-su@users.noreply.github.com>
2024-02-23 19:07:31 -0800

    VN game branching options

--- game/Argon/Assets/Assets/Scriptable Objects/StoryControllers/VNGameController.cs
+++ game/Argon/Assets/Assets/Scriptable Objects/StoryControllers/VNGameController.cs
@@ -1,41 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class VNGameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-
-    public StoryScene currScene;
+    public GameScene currentScene;
     public BottomBarController bottomBar;
     public BackgroundController backgroundController;
-    void Start()
+    public OptionsLogicController optionsController;
+
+    private State state = State.IDLE;
+
+    private enum State
     {
-        bottomBar.PlayScene(currScene);
-        backgroundController.SetImage(currScene.background);
+        IDLE, ANIMATE, CHOOSE
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        if (currentScene is StoryScene)
+        {
+            StoryScene storyScene = currentScene as StoryScene;
+            bottomBar.PlayScene(storyScene);
+            backgroundController.SetImage(storyScene.background);
+        }
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
-            if (bottomBar.IsCompleted())
+            if (state == State.IDLE && bottomBar.IsCompleted())
             {
                 if (bottomBar.IsLastSentence())
                 {
-                    currScene = currScene.nextScene;
-                    bottomBar.PlayScene(currScene);
-                    backgroundController.SwitchImage(currScene.background);
+                    PlayScene((currentScene as StoryScene).nextScene);
                 }
                 else
                 {
                     bottomBar.PlayNextSentence();
                 }
-                
             }
+        }
+    }
+
+    public void PlayScene(GameScene scene)
+    {
+        StartCoroutine(SwitchScene(scene));
+    }
+
+    private IEnumerator SwitchScene(GameScene scene)
+    {
+        state = State.ANIMATE;
+        currentScene = scene;
+        bottomBar.Hide();
+        yield return new WaitForSeconds(1f);
+        if (scene is StoryScene)
+        {
+            StoryScene storyScene = scene as StoryScene;
+            backgroundController.SwitchImage(storyScene.background);
+            yield return new WaitForSeconds(1f);
+            bottomBar.ClearText();
+            bottomBar.Show();
+            yield return new WaitForSeconds(1f);
+            bottomBar.PlayScene(storyScene);
+            state = State.IDLE;
+        }
+        else if (scene is ChooseScene)
+        {
+            state = State.CHOOSE;
+            optionsController.SetupChoose(scene as ChooseScene);
         }
     }
 }
