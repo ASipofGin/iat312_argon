5ca4b7d34e53413fdabed658e036b8ef9ca22698
jeffrey-su <137955909+jeffrey-su@users.noreply.github.com>
2024-03-03 20:51:05 -0800

    added npc dialogue

---
+++ game/Argon/Assets/NPCBehaviour.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NPCBehaviour : MonoBehaviour
+{
+
+    public GameObject dialoguePanel;
+    public Text dialogueText;
+    public Text npcName;
+    public string[] dialogue;
+    public string name;
+    private int index;
+
+    public float textSpeed;
+    public bool playerProximity;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerProximity)
+        {
+            if (dialoguePanel.activeInHierarchy)
+            {
+                zeroText();
+            }
+            else
+            {
+                
+                dialoguePanel.SetActive(true);
+                npcName.text = name;
+                StartCoroutine(TextEffect());
+            }
+        }
+    }
+
+    public void zeroText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+
+    IEnumerator TextEffect()
+    {
+        foreach(char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        if(index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(TextEffect());
+        }
+        else
+        {
+            zeroText();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerProximity = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerProximity = false;
+            zeroText();
+        }
+    }
+
+}
