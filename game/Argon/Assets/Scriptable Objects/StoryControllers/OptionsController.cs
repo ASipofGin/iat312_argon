b730d1b95909c37086b0a4ce797acdda0bd242d9
ASipofGin <andyzhang.school@gmail.com>
2024-03-18 01:42:29 -0700

    Text fix

--- game/Argon/Assets/Scriptable Objects/StoryControllers/OptionsController.cs
+++ game/Argon/Assets/Scriptable Objects/StoryControllers/OptionsController.cs
@@ -9,6 +9,7 @@ public class OptionsController : MonoBehaviour, IPointerClickHandler, IPointerEn
     private StoryScene scene;
     private TextMeshProUGUI textMesh;
     private OptionsLogicController controller;
+    private bool isClicked = false; // Debounce flag
 
     void Awake()
     {
@@ -30,11 +31,18 @@ public class OptionsController : MonoBehaviour, IPointerClickHandler, IPointerEn
         Vector3 position = textMesh.rectTransform.localPosition;
         position.y = y;
         textMesh.rectTransform.localPosition = position;
+        
+        isClicked = false; // Reset debounce flag when setting up
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        controller.PerformChoose(scene);
+        if (!isClicked) // Check if not already clicked
+        {
+            isClicked = true; // Set flag to true to block further clicks
+            controller.PerformChoose(scene);
+            // Optionally, reset isClicked to false after some time or after certain conditions are met
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
